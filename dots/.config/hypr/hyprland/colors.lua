148a82898b74f3e57542a2f8e5ff8da2944ceb08
Moci-cu <pekztor4gung@gmail.com>
2026-09-06 18:50:36 +0800

    style: set inactive window border opacity to 85 percent

--- dots/.config/hypr/hyprland/colors.lua
+++ dots/.config/hypr/hyprland/colors.lua
@@ -2,7 +2,7 @@ hl.config({
     general = {
         col = {
             active_border   = "rgba(44464fFF)",
-            inactive_border = "rgba(1a1b2033)",
+            inactive_border = "rgba(1a1b20D9)",
         },
     },
     misc = {
